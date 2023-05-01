07de3d0e614e2defc253f8bbe89cc0348cb173e9
hlee <reynalee@gmail.com>
2023-05-01 09:55:57 -0400

    5/1

--- chicago_database.sql
+++ chicago_database.sql
@@ -11,7 +11,7 @@ CREATE TABLE crimes (
     location_description varchar,
     arrest boolean,
     domestic boolean,
-    beat ineger,
+    beat integer,
     district integer,
     ward integer,
     community_area integer,
@@ -23,21 +23,20 @@ CREATE TABLE crimes (
     latitude numeric,
     longitude numeric,
     location varchar,
-    :@computed_region_awaf_s7ux integer,
-    :@computed_region_6mkv_f3dw integer,
-    :@computed_region_vrxf_vc4k integer,
-    :@computed_region_bdys_3d7i integer,
-    :@computed_region_43wa_7qmu integer,
-    :@computed_region_rpca_8um6 integer,
-    :@computed_region_d9mm_jgwp integer,
-    :@computed_region_d3ds_rm58 integer
+    computed_region_awaf_s7ux integer,
+    computed_region_6mkv_f3dw integer,
+    computed_region_vrxf_vc4k integer,
+    computed_region_bdys_3d7i integer,
+    computed_region_43wa_7qmu integer,
+    computed_region_rpca_8um6 integer,
+    computed_region_d9mm_jgwp integer,
+    computed_region_d3ds_rm58 integer
 );
 
-\copy  crime (id, case_number, date, block, iucr, primary_type, description, location_description, arrest, domestic, beat, district, ward, community_area, fbi_code, x_coordinate, y_coordinate, year, updated_on, latitude, longitude, location, :@computed_region_awaf_s7ux, :@computed_region_6mkv_f3dw, :@computed_region_vrxf_vc4k, :@computed_region_bdys_3d7i, :@computed_region_43wa_7qmu, :@computed_region_rpca_8um6, :@computed_region_d9mm_jgwp, :@computed_region_d3ds_rm58) FROM '.\data\crime.csv' WITH (FORMAT csv, HEADER);
+\copy  crimes (id, case_number, date, block, iucr, primary_type, description, location_description, arrest, domestic, beat, district, ward, community_area, fbi_code, x_coordinate, y_coordinate, year, updated_on, latitude, longitude, location, computed_region_awaf_s7ux, computed_region_6mkv_f3dw, computed_region_vrxf_vc4k, computed_region_bdys_3d7i, computed_region_43wa_7qmu, computed_region_rpca_8um6, computed_region_d9mm_jgwp, computed_region_d3ds_rm58) FROM '.\data\crime.csv' WITH (FORMAT csv, HEADER);
 
 
 CREATE TABLE clinics (
-    id integer PRIMARY KEY,
     organization varchar,
     site_name varchar,
     site_type varchar,
@@ -45,15 +44,15 @@ CREATE TABLE clinics (
     zip varchar,
     main_phone varchar,
     service_request_phone varchar,
-    evening_hours_availability boolearn,
+    evening_hours_availability boolean,
     evening_hours_detail varchar,
-    weekend_hours_availability boolearn,
+    weekend_hours_availability boolean,
     ticc_site varchar,
     all_community_areas_served varchar,
     wards_served varchar,
     police_districts_served varchar,
     accepts_uninsured_patients boolean,
-    sliding_scale_fees boolean,
+    sliding_scale_fees varchar,
     insurance_accepted varchar,
     age_range_served varchar,
     primary_care_availability boolean,
@@ -69,11 +68,11 @@ CREATE TABLE clinics (
     first_episode_psychosis_programming_availability boolean,
     substance_use_services_availability boolean,
     location varchar,
-    :@computed_region_rpca_8um6 integer,
-    :@computed_region_vrxf_vc4k integer,
-    :@computed_region_6mkv_f3dw integer,
-    :@computed_region_bdys_3d7i integer,
-    :@computed_region_43wa_7qmu integer,
+    computed_region_rpca_8um6 integer,
+    computed_region_vrxf_vc4k integer,
+    computed_region_6mkv_f3dw integer,
+    computed_region_bdys_3d7i integer,
+    computed_region_43wa_7qmu integer,
     weekend_hours_detail varchar,
     grief_bereavement_services_types varchar,
     substance_use_services_type varchar,
@@ -91,13 +90,11 @@ CREATE TABLE clinics (
     first_episode_psychosis_programming_detail varchar
 );
 
-\copy  clinics (organization, site_name, site_type, address, zip, main_phone, service_request_phone, evening_hours_availability, evening_hours_detail, weekend_hours_availability, ticc_site, all_community_areas_served, wards_served, police_districts_served, accepts_uninsured_patients, sliding_scale_fees, insurance_accepted, age_range_served primary_care_availability, bhs_service_availability_primary_care, telehealth_services_availability, other_languages, language_line_availability, behavioral_services_types, crisis_services, mental_health_services_type_special_needs_availability, school_based_services_availability, grief_services_availability, first_episode_psychosis_programming_availability, substance_use_services_availability, location, :@computed_region_rpca_8um6, :@computed_region_vrxf_vc4k, :@computed_region_6mkv_f3dw, :@computed_region_bdys_3d7i, :@computed_region_43wa_7qmu, weekend_hours_detail, grief_bereavement_services_types, substance_use_services_type, mar_medications_opioid, harm_reduction_services, service_request_alternative_availability, service_request_alternative_detail, hours, mental_health_services_type_special_needs_detail, ticc_areas_served, mar_medications_alcohol, crisis_intake_phone, crisis_services_hours, crisis_services_types, first_episode_psychosis_programming_detail
-) FROM '.\data\clinics.csv' WITH (FORMAT csv, HEADER);
+\copy  clinics (organization, site_name, site_type, address, zip, main_phone, service_request_phone, evening_hours_availability, evening_hours_detail, weekend_hours_availability, ticc_site, all_community_areas_served, wards_served, police_districts_served, accepts_uninsured_patients, sliding_scale_fees, insurance_accepted, age_range_served, primary_care_availability, bhs_service_availability_primary_care, telehealth_services_availability, other_languages, language_line_availability, behavioral_services_types, crisis_services, mental_health_services_type_special_needs_availability, school_based_services_availability, grief_services_availability, first_episode_psychosis_programming_availability, substance_use_services_availability, location, computed_region_rpca_8um6, computed_region_vrxf_vc4k, computed_region_6mkv_f3dw, computed_region_bdys_3d7i, computed_region_43wa_7qmu, weekend_hours_detail, grief_bereavement_services_types, substance_use_services_type, mar_medications_opioid, harm_reduction_services, service_request_alternative_availability, service_request_alternative_detail, hours, mental_health_services_type_special_needs_detail, ticc_areas_served, mar_medications_alcohol, crisis_intake_phone, crisis_services_hours, crisis_services_types, first_episode_psychosis_programming_detail) FROM '.\data\clinics.csv' WITH (FORMAT csv, HEADER);
 
-CREATE TABLE data.div (
-    id	integer	PRIMARY KEY,
-    case_id	integer	REFERENCE crimes,
-    case_participant_id	integer REFERENCE crimes,
+CREATE TABLE data_div (
+    case_id	bigint	REFERENCES crimes,
+    case_participant_id	bigint,
     received_date date,
     offense_category varchar,	
     diversion_program varchar,	
@@ -111,12 +108,12 @@ CREATE TABLE data.div (
     diversion_closed_date date
 );
 
-\copy  data.div (id, case_id, case_participant_id, received_date, offense_category, diversion_program, referral_date, diversion_count, primary_charge_offense_title, statute, race, gender, diversion_result, diversion_closed_date) FROM '.\data.div.csv' WITH (FORMAT csv, HEADER);
+\copy  data_div (case_id, case_participant_id, received_date, offense_category, diversion_program, referral_date, diversion_count, primary_charge_offense_title, statute, race, gender, diversion_result, diversion_closed_date) FROM '.data\data_div.csv' WITH (FORMAT csv, HEADER);
 
 CREATE TABLE div (
-    id	integer	PRIMARY KEY,
-    case_id	integer	REFERENCE crimes,
-    case_participant_id	integer REFERENCE crimes,
+    id integer PRIMARY KEY,
+    case_id	integer	REFERENCES crimes,
+    case_participant_id	integer REFERENCES crimes,
     received_date date,
     offense_category varchar,	
     diversion_program varchar,	
@@ -135,8 +132,8 @@ CREATE TABLE div (
 
 CREATE TABLE disp (
     id	integer	PRIMARY KEY,
-    case_id	integer	REFERENCE,
-    case_participant_id	integer REFERENCE crimes,	
+    case_id	integer	REFERENCES crimes,
+    case_participant_id	integer REFERENCES crimes,	
     received_date date,	
     offense_category varchar,	
     primary_charge varchar,	
@@ -148,23 +145,23 @@ CREATE TABLE disp (
     disposition_charged_chapter	varchar,	
     disposition_charged_act	integer,	
     disposition_charged_section	varchar,	
-    disposition_charged_class	varchar,	
-    disposition_charged_aoic	integer,	
-    charge_disposition	varchar,	
-    judge	varchar,	
-    court_name	varchar,	
+    disposition_charged_class varchar,	
+    disposition_charged_aoic integer,	
+    charge_disposition varchar,	
+    judge varchar,	
+    court_name varchar,	
     court_facility	varchar,	
     age_at_incident	integer,	
-    race	varchar,	
-    gender	varchar,	
+    race varchar,	
+    gender varchar,	
     incident_begin_date	date,	
     law_enforcement_agency	varchar,	
     arrest_date	date,	
     felony_review_date	date,	
-    felony_review_result	date,	
-    arraignment_date	date,	
+    felony_review_result date,	
+    arraignment_date date,	
     updated_offense_category	varchar,	
-    charge_disposition_reason,		
+    charge_disposition_reason varchar,		
     incident_end_date	date,	
     incident_city	varchar,	
     unit	varchar
@@ -175,8 +172,8 @@ CREATE TABLE disp (
 
 CREATE TABLE init (
     id	integer	PRIMARY KEY,
-    case_id	integer	REFERENCE,
-    case_participant_id	integer REFERENCE crimes,	
+    case_id	integer	REFERENCES crimes,
+    case_participant_id	integer REFERENCES crimes,	
     received_date date,	
     offense_category varchar,	
     primary_charge varchar,	
@@ -204,7 +201,7 @@ CREATE TABLE init (
     felony_review_result	date,	
     arraignment_date	date,	
     updated_offense_category	varchar,	
-    charge_disposition_reason,		
+    charge_disposition_reason varchar,		
     incident_end_date	date,	
     incident_city	varchar,	
     unit	varchar
@@ -215,8 +212,8 @@ CREATE TABLE init (
 
 CREATE TABLE intake (
     id	integer	PRIMARY KEY,
-    case_id	integer	REFERENCE,
-    case_participant_id	integer REFERENCE crimes,	
+    case_id	integer	REFERENCES crimes,
+    case_participant_id	integer REFERENCES crimes,	
     received_date date,	
     offense_category varchar,	
     primary_charge varchar,	
@@ -244,18 +241,19 @@ CREATE TABLE intake (
     felony_review_result	date,	
     arraignment_date	date,	
     updated_offense_category	varchar,	
-    charge_disposition_reason,		
+    charge_disposition_reason varchar,		
     incident_end_date	date,	
     incident_city	varchar,	
     unit	varchar
 );
+
 \copy  intake (id,	case_id, case_participant_id, received_date, offense_category, primary_charge, charge_id, charge_version_id, disposition_charged_offense_title, charge_count, disposition_date, disposition_charged_chapter, disposition_charged_act, disposition_charged_section, disposition_charged_class, disposition_charged_aoic, charge_disposition, judge, court_name, court_facility, age_at_incident, race, gender, incident_begin_date, law_enforcement_agency, arrest_date, felony_review_date, felony_review_result, arraignment_date, updated_offense_category, charge_disposition_reason, incident_end_date, incident_city, unit) FROM '.\intake.csv' WITH (FORMAT csv, HEADER);
 
 
 CREATE TABLE sent (
     id	integer	PRIMARY KEY,
-    case_id	integer	REFERENCE,
-    case_participant_id	integer REFERENCE crimes,	
+    case_id	integer	REFERENCES crimes,
+    case_participant_id	integer REFERENCES crimes,	
     received_date date,	
     offense_category varchar,	
     primary_charge varchar,	
@@ -283,37 +281,48 @@ CREATE TABLE sent (
     felony_review_result	date,	
     arraignment_date	date,	
     updated_offense_category	varchar,	
-    charge_disposition_reason,		
+    charge_disposition_reason varchar,		
     incident_end_date	date,	
     incident_city	varchar,	
     unit	varchar
-);	
+);
 
 \copy  sent (id,	case_id, case_participant_id, received_date, offense_category, primary_charge, charge_id, charge_version_id, disposition_charged_offense_title, charge_count, disposition_date, disposition_charged_chapter, disposition_charged_act, disposition_charged_section, disposition_charged_class, disposition_charged_aoic, charge_disposition, judge, court_name, court_facility, age_at_incident, race, gender, incident_begin_date, law_enforcement_agency, arrest_date, felony_review_date, felony_review_result, arraignment_date, updated_offense_category, charge_disposition_reason, incident_end_date, incident_city, unit) FROM '.\sent.csv' WITH (FORMAT csv, HEADER);
 
 
-CREATE TABLE HCSBHP (
-    id	integer	PRIMARY KEY,
+CREATE TABLE HCSBDP (
     se	numeric,	
     g	varchar,	
     l	varchar,	
     a	varchar,	
     p	varchar,	
-    d	date,	
+    d	varchar,	
     v	numeric	
 );
 
-\copy  HCSBHP (id, se, g, l, a, p, d, v) FROM '.\HCSBHP.csv' WITH (FORMAT csv, HEADER);
+\copy  HCSBDP (se, g, l, a, p, d, v) FROM '.\HCSBDP.csv'WITH (FORMAT csv, HEADER);
+iconv -c -t utf8 C:\Users\HyeOL\OneDrive - CUNY\GitHub\project_4\chicago_mh\data\HCSBDP.csv > C:\Users\HyeOL\OneDrive - CUNY\GitHub\project_4\chicago_mh\data\HCSBDP.utf8.csv
 
-
-CREATE TABLE HDBHR (
-    id	integer	PRIMARY KEY,
+CREATE TABLE HCSBHP (
     se	numeric,	
     g	varchar,	
     l	varchar,	
     a	varchar,	
     p	varchar,	
-    d	date,	
+    d	varchar,	
+    v	numeric	
+);
+
+\copy  HCSBHP (se, g, l, a, p, d, v) FROM '.\HCSBHP.csv' WITH (FORMAT csv, HEADER);
+
+
+CREATE TABLE HDBHR (
+    se	numeric,	
+    g	varchar,	
+    l	varchar,	
+    a	varchar,	
+    p	varchar,	
+    d	varchar,
     v	numeric	
 );
 
@@ -321,31 +330,29 @@ CREATE TABLE HDBHR (
 
 
 CREATE TABLE HDDRR (
-    id	integer	PRIMARY KEY,
     se	numeric,	
     g	varchar,	
     l	varchar,	
     a	varchar,	
     p	varchar,	
-    d	date,	
+    d	varchar,	
     v	numeric	
 );
 
-\copy  HDDRR (id, se, g, l, a, p, d, v) FROM '.\HDDRR.csv' WITH (FORMAT csv, HEADER);
+\copy  HDDRR (se, g, l, a, p, d, v) FROM '.\HDDRR.csv' WITH (FORMAT csv, HEADER);
 
 
 CREATE TABLE HDSZR (
-    id	integer	PRIMARY KEY,
     se	numeric,	
     g	varchar,	
     l	varchar,	
     a	varchar,	
     p	varchar,	
-    d	date,	
+    d	varchar,	
     v	numeric	
 );
 
-\copy  HDSZR (id, se, g, l, a, p, d, v) FROM '.\HDSZR.csv' WITH (FORMAT csv, HEADER);
+\copy  HDSZR (se, g, l, a, p, d, v) FROM '.\HDSZR.csv' WITH (FORMAT csv, HEADER);
 
 
 CREATE TABLE YRDEP (
